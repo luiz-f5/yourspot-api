b5c2e1574c0883fb341869aecae235157f038748
joaozinhoDoGrauXd <joaoninguem1301@gmail.com>
2026-07-19 14:37:37 -0300

    chore: att dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, 
+    new FastifyAdapter({logger: true}));
+  await app.listen( 3000, '0.0.0.0');
 }
 bootstrap();
